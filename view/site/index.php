410a5767969511fd780f5a3bd5a3085e4b50dcc3
Adamovich Dima <program.malik@gmail.com>
2018-04-11 23:44:58 +0300

    Refactring structure sites

--- view/site/index.php
+++ view/site/index.php
@@ -1,22 +1,12 @@
-    <div class="col s12 m3">
-        <div class="left-panel">
-            <div class="left-panel-header ">
-                Погода в Пулинах
-            </div>
-            <div class="left-panel-content">
-                <div id="SinoptikInformer" class="SinoptikInformer type1c1"><div class="siHeader"><div class="siLh"><div class="siMh"><a onmousedown="siClickCount();" class="siLogo" href="https://ua.sinoptik.ua/" target="_blank" rel="nofollow" title="Погода"> </a>Погода <span id="siHeader"></span></div></div></div><div class="siBody"><a onmousedown="siClickCount();" href="https://ua.sinoptik.ua/погода-пулини" title="Погода у Пулинах" target="_blank"><div class="siCity"><div class="siCityName"><span>Пулини</span></div><div id="siCont0" class="siBodyContent"><div class="siLeft"><div class="siTerm"></div><div class="siT" id="siT0"></div><div id="weatherIco0"></div></div><div class="siInf"><p>вологість: <span id="vl0"></span></p><p>тиск: <span id="dav0"></span></p><p>вітер: <span id="wind0"></span></p></div></div></div></a><div class="siLinks">Погода на 10 днів від <a href="https://ua.sinoptik.ua/погода-пулини/10-днів" title="Погода на 10 днів" target="_blank" onmousedown="siClickCount();">sinoptik.ua</a></div></div><div class="siFooter"><div class="siLf"><div class="siMf"></div></div></div></div><script type="text/javascript" charset="UTF-8" src="//sinoptik.ua/informers_js.php?title=4&amp;wind=3&amp;cities=303028575&amp;lang=ua"></script>
-            </div>
+<div class="col s12 m9">
+    <div class="card">
+        <div class="card-image">
+            <img src="<?=HOST?>/template/img/sample-1.jpg">
+            <span class="card-title">Card Title</span>
+            <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+        </div>
+        <div class="card-content">
+            <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
         </div>
     </div>
-    <div class="col s12 m9">
-        <div class="card">
-            <div class="card-image">
-                <img src="<?=HOST?>/template/img/sample-1.jpg">
-                <span class="card-title">Card Title</span>
-                <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
-            </div>
-            <div class="card-content">
-                <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
-            </div>
-        </div>
-    </div>
+</div>

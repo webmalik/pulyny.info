1ac755d5991f565ee08e8c03cf902951467aa2bd
Adamovich Dima <program.malik@gmail.com>
2018-04-11 23:09:11 +0300

    Add new styles in footer and content

--- view/main.php
+++ view/main.php
@@ -8,9 +8,9 @@
     <!-- Title -->
     <title>Пулини.info</title>
     <!-- Styles -->
-    <link href="<?=HOST?>/template/css/style.css" rel="stylesheet">
     <link href="<?=HOST?>/template/css/font-awesome.min.css" rel="stylesheet">
     <link href="<?=HOST?>/template/css/materialize.min.css" rel="stylesheet">
+    <link href="<?=HOST?>/template/css/style.css" rel="stylesheet">
 </head>
 <body>
 <div class="container-fluid">
@@ -68,16 +68,36 @@
 
     </div>
 
-    <div class="footer">
-        <div class="row z-depth-2">
-            <div class="col s12 m6 left-align">
-                &copy <?php echo date('Y')?> Copyright Text
-            </div>
-            <div class="col s12 m6 right-align">
-                Powered by <a href="#">WebMaLik</a>
+
+    <footer class="page-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col l6 s12">
+                    <h5 class="white-text">Footer Content</h5>
+                    <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+                </div>
+                <div class="col l4 offset-l2 s12">
+                    <h5 class="white-text">Links</h5>
+                    <ul>
+                        <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                        <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                        <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                        <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                    </ul>
+                </div>
             </div>
         </div>
-    </div>s
+        <div class="footer">
+            <div class="row z-depth-2">
+                <div class="col s12 m6 left-align">
+                    &copy <?php echo date('Y')?> Copyright Text
+                </div>
+                <div class="col s12 m6 right-align">
+                    Powered by <a href="#">WebMaLik</a>
+                </div>
+            </div>
+        </div>
+    </footer>
 </div>
 
 <!-- SCRIPTS -->
--- view/site/index.php
+++ view/site/index.php
@@ -1,52 +1,14 @@
-    <div class="col s12 m6">
-        <div class="card">
-            <div class="card-image">
-                <img src="<?=HOST?>/template/img/sample-1.jpg">
-                <span class="card-title">Card Title</span>
-                <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+    <div class="col s12 m3">
+        <div class="left-panel">
+            <div class="left-panel-header">
+
             </div>
-            <div class="card-content">
-                <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+            <div class="left-panel-content">
+
             </div>
         </div>
     </div>
-    <div class="col s12 m6">
-        <div class="card">
-            <div class="card-image">
-                <img src="<?=HOST?>/template/img/sample-1.jpg">
-                <span class="card-title">Card Title</span>
-                <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
-            </div>
-            <div class="card-content">
-                <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
-            </div>
-        </div>
-    </div>
-    <div class="col s12 m6">
-        <div class="card">
-            <div class="card-image">
-                <img src="<?=HOST?>/template/img/sample-1.jpg">
-                <span class="card-title">Card Title</span>
-                <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
-            </div>
-            <div class="card-content">
-                <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
-            </div>
-        </div>
-    </div>
-    <div class="col s12 m6">
-        <div class="card">
-            <div class="card-image">
-                <img src="<?=HOST?>/template/img/sample-1.jpg">
-                <span class="card-title">Card Title</span>
-                <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
-            </div>
-            <div class="card-content">
-                <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
-            </div>
-        </div>
-    </div>
-    <div class="col s12 m6">
+    <div class="col s12 m9">
         <div class="card">
             <div class="card-image">
                 <img src="<?=HOST?>/template/img/sample-1.jpg">

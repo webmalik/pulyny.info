4004f17f3e68cd71c290519bf8fff4aa32200411
Adamovich Dima <program.malik@gmail.com>
2018-04-22 15:47:58 +0300

    Restyle admin panel@

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -5,7 +5,7 @@ class AdminController //extends FrontController
     public function actionIndex() {
 
         $template = new Template();
-        $template->adminrender('about/index');
+        $template->adminrender('admin/index');
         return true;
     }
 }
